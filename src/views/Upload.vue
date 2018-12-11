aa5ddf67bd3e9f59efa3b6c80152da057c6d56bf
John Arkema <john.arkema@gmail.com>
2018-12-11 13:44:03 -0600

    Upload and user views

---
+++ src/views/Upload.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="Upload">
+     <font-awesome-icon :icon="['fas', 'file-upload']" size="lg" />
+  </div>
+</template>
+<script>
+  export default {
+  name: 'Upload',
+  components: {
+    
+  },
+  data() {
+    return {
+    }
+  }
+}
+</script>
+<style scoped lang="scss">
+  
+</style>
