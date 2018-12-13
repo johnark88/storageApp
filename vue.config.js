6d10f3dd876a6d3a515eaba6beb9ff4f012ada91
John Arkema <john.arkema@gmail.com>
2018-12-13 12:27:40 -0600

    Add global variables for sass - via webpack config

--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,9 @@
 module.exports = {
-  // chainWebpack: (config) => {
-  //   config.module
-  //     .rule('scss')
-  //     .use('sass-loader')
-  //     .loader('sass-loader')
-  //     .tap((options) => {
-  //       // modify sass options ie $color: red;
-  //       data: `$colorWhite: #F7F4F3;`
-  //       return options;
-  //     });
-  // },
+  css: {
+    loaderOptions: {
+      sass: {
+        data: '@import "@/scss/_variables.scss";',
+      },
+    },
+  },
 };
