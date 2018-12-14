6a353515dd3e99496593f29fc93e95f566c9e045
John Arkema <john.arkema@gmail.com>
2018-12-14 15:26:56 -0600

    Added new icon

--- src/plugins/fontawesome.js
+++ src/plugins/fontawesome.js
@@ -1,5 +1,6 @@
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { faFolderOpen, faFileUpload } from '@fortawesome/free-solid-svg-icons';
+import { faFolderOpen, faFileUpload, faFile } from '@fortawesome/free-solid-svg-icons';
 
 library.add(faFolderOpen);
 library.add(faFileUpload);
+library.add(faFile);
