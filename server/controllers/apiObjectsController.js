43c5fd472ca2fd87014aec5a1eb0a4ae5a185ff4
John Arkema <john.arkema@gmail.com>
2018-12-14 15:28:30 -0600

    Moved sorting and duplicate code to back end

--- server/controllers/apiObjectsController.js
+++ server/controllers/apiObjectsController.js
@@ -8,7 +8,41 @@ const storage = new Storage ({
 });
 
 exports.getObjects = async (req, res) => {
+
+  function findDuplicates(theFolders) {
+    // Sort out all duplications
+    // push remaining folders to result array
+    const result = [];
+    theFolders.forEach((element, index) => {
+      // Find if there is a duplicate or not
+      if (theFolders.indexOf(element, index + 1) > -1) {
+        // Find if the element is already in the result array or not
+        if (result.indexOf(element) === -1) {
+          // pa pa push it real good ....... to the results array
+          result.push(element);
+        }
+      }
+    });
+    // send to frontend
+    res.json(result);
+  }
+
+  // sort through array to find only the folders
+  function sortFolders(folderList) {
+    const foldersSorted = [];
+    folderList.files.forEach((folder) => {
+      // break apart files from folders
+      const stringSplit = folder.split('/')[0];
+      // push folders to array
+      foldersSorted.push(stringSplit);
+    });
+    // send to function to remove duplicates
+    findDuplicates(foldersSorted);
+  }
+
+  // Gets ALL items in bucket
   const bucketName = req.params.bucketName;
+  // the get
   const [files] = await storage.bucket(bucketName).getFiles();
   const bucketObjects = {
     bucket: bucketName,
@@ -17,5 +51,42 @@ exports.getObjects = async (req, res) => {
   files.forEach((file) => {
     bucketObjects.files.push(file.name);
   });
-  res.json(bucketObjects);
+  // send to sorter to get folders
+  sortFolders(bucketObjects);
+};
+
+exports.getFileObjects = async (req, res) => {
+  const bucketName = req.params.storage;
+  const prefix = req.params.name+'/';
+  const delimiter = '/';
+  // const filePrefix = bucketName+'/'+req.params.name;
+
+  function sortFiles(objectList) {
+    const filesSorted = [];
+    objectList.forEach((file) => {
+      // Breaks off the prefix
+      const stringSplit = file.split('/')[1];
+      // push it to temp array
+      filesSorted.push(stringSplit);
+      if (filesSorted[0] === '') {
+        filesSorted.shift();
+      }
+    });
+    res.json(filesSorted);
+  }
+
+  const options = {
+    prefix: prefix,
+  };
+
+  if (delimiter) {
+    options.delimiter = delimiter;
+  }
+  // Lists files in the bucket, filtered by a prefix
+  const [files] = await storage.bucket(bucketName).getFiles(options);
+  const filesToSort = [];
+  files.forEach((file) => {
+    filesToSort.push(file.name);
+  });
+  sortFiles(filesToSort);
 };
--- server/routes/apiRoutes.js
+++ server/routes/apiRoutes.js
@@ -7,20 +7,13 @@ module.exports = ((app) => {
     .post(apiList.createBucket);
   // .delete()
 
-  // Objects
   app.route('/api/objects/:bucketName')
+    // gets folders
     .get(apiObjectList.getObjects);
   //   .post(apiList.upLoadObjects)
   //   .get(apiList.downLoadObjects);
   // .delete()
-  // app.use((err, req, res, next) => {
-  //   // Log error message in our server's console
-  //   console.error(err, 'server');
-  //   // If err has no specified error code, set error code to 'Internal Server Error (500)'
-  //   if (!err.statusCode) {
-  //     err.statusCode = 404;
-  //   }
-  //   // All HTTP requests must have a response, so let's send back an error with its status code and message
-  //   res.status(err.statusCode).send(err.message);
-  // });
+
+  app.route('/api/objects/files/:storage/:name')
+    .get(apiObjectList.getFileObjects);
 });
